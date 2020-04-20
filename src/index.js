61ce52ce5a1f317c7624cc72657e8a441b6e23c6
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-04-20 05:44:03 -0300

    Implementa função fibonacci

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 'use strict'
 
-const fibonacci = () => null
+const fibonacci = () => {
+  const fibonacciNumbers = [0, 1]
+
+  while (fibonacciNumbers[fibonacciNumbers.length - 1] <= 350) {
+    const lastNumber = fibonacciNumbers[fibonacciNumbers.length - 1]
+    const penultimateNumber = fibonacciNumbers[fibonacciNumbers.length - 2]
+    const nextNum = lastNumber + penultimateNumber
+
+    fibonacciNumbers.push(nextNum)
+  }
+
+  return fibonacciNumbers
+}
 
 const isFibonnaci = (num) => null
 
 module.exports = {
-    fibonacci,
-    isFibonnaci
+  fibonacci,
+  isFibonnaci,
 }
