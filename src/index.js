b5df45f57cec3089c2ea1b0541f12da23953416d
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-04-20 06:24:41 -0300

    Corrige indentação

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@
 
 /// Returns the fibonacci sequence
 const fibonacci = () => {
-	// init fibonacci array
+  // init fibonacci array
   const fibonacciNumbers = [0, 1]
 
-	// keep adding numbers until it passes 350
+  // keep adding numbers until it passes 350
   while (fibonacciNumbers[fibonacciNumbers.length - 1] <= 350) {
     const lastNumber = fibonacciNumbers[fibonacciNumbers.length - 1]
     const penultimateNumber = fibonacciNumbers[fibonacciNumbers.length - 2]
     const nextNum = lastNumber + penultimateNumber
 
-		// add number to the fibonacci array
+    // add number to the fibonacci array
     fibonacciNumbers.push(nextNum)
   }
 
@@ -20,14 +20,14 @@ const fibonacci = () => {
 
 /// Checks if a number is a fibonacci number
 const isFibonnaci = (num) => {
-	// calculate expressions
+  // calculate expressions
   const firstExpression = Math.sqrt(5 * Math.pow(num, 2) + 4)
-	const secondExpression = Math.sqrt(5 * Math.pow(num, 2) - 4)
-	// check if expressions return an integer
+  const secondExpression = Math.sqrt(5 * Math.pow(num, 2) - 4)
+  // check if expressions return an integer
   const firstCheck = Number.isInteger(firstExpression)
   const secondCheck = Number.isInteger(secondExpression)
 
-	// check if any of the expressions is true
+  // check if any of the expressions is true
   if (firstCheck || secondCheck) {
     return true
   }
